f072a738fdb09ff075ba785b7a15e2fa46d00826
Andrew Southwick <asouthwick01@gmail.com>
2018-03-22 18:06:11 -0600

    autoplay video

--- src/components/heroBar.js
+++ src/components/heroBar.js
@@ -11,7 +11,7 @@ render(){
           <div className="d-flex">
             <img src="images/logo.png" alt=""/> <h1>Register For A Webinar</h1>
           </div>
-      <iframe autoPlay src="https://player.vimeo.com/video/253155284?title=0&byline=0&portrait=0" width="100%" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          <iframe src="https://player.vimeo.com/video/253155284?autoplay=1&title=0&byline=0&portrait=0" width="100%" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
         </Col>
         <Col xs="4">
           <h2>Day Trading Intro Webinar</h2>
