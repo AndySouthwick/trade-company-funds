6089b6d01306f171bd77c003f718ad2515b23419
Andrew Southwick <Andrew.Southwick@us.usana.com>
2018-04-23 15:34:05 -0600

    changed to jeffs name

--- src/components/sectionOne.js
+++ src/components/sectionOne.js
@@ -95,8 +95,7 @@ class SectionOne extends Component {
              <p>Need to contact us? No Problem, let us know how we can help.
                Office</p>
 
-              <p>Dan: 385-444-6555 dan@gotradeourmoney.com</p>
-
+              <p>Jeff: (801)-895-4541</p>
               <p>Monday – Friday, 9am – 5pm MST</p>
 
               <p>Mailing Address</p>
